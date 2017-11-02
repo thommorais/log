aa65701c62bc7d4268b8af063a993db5591f1571
Josh Avanier <joshavanier@gmail.com>
2017-11-02 18:26:41 +0700

    Clean

--- js/options.js
+++ js/options.js
@@ -20,7 +20,6 @@ Log.options = {
   setBG(c) {
     shell.sed("-i", /^.*bg:.*$/, `\t\tbg: "${c}",`, "data/config.js")
     Log.config.ui.bg = c
-    // document.getElementById("app").style.backgroundColor = c
   },
 
   /**
@@ -31,7 +30,6 @@ Log.options = {
   setColour(c) {
     shell.sed("-i", /^.*colour:.*$/, `\t\tcolour: "${c}",`, "data/config.js")
     Log.config.ui.colour = c
-    // document.getElementById("app").style.color = c
   },
 
   /**
@@ -43,7 +41,7 @@ Log.options = {
     shell.sed("-i", /^.*accent:.*$/, `\t\taccent: "${c}",`, "data/config.js")
     Log.config.ui.accent = c
   },
-  
+
   /**
    * Set interface font family
    * @param {string} f - The font family
@@ -52,7 +50,6 @@ Log.options = {
   setFont(font) {
     shell.sed("-i", /^.*font:.*$/, `\t\tfont: "${f}",`, "data/config.js")
     Log.config.ui.font = f
-    // document.getElementById("app").style.fontFamily = f
   },
 
   /**
@@ -63,7 +60,6 @@ Log.options = {
   setIcons(a) {
     shell.sed("-i", /^.*icons:.*$/, `\t\ticons: ${a},`, "data/config.js")
     Log.config.ui.icons = a
-    // Log.refresh()
   },
 
   /**
@@ -74,7 +70,6 @@ Log.options = {
   setView(n) {
     shell.sed("-i", /^.*view:.*$/, `\t\tview: ${n},`, "data/config.js")
     Log.config.ui.view = n
-    // Log.refresh()
   },
 
   /**
@@ -85,7 +80,6 @@ Log.options = {
   setCalendar(c) {
     shell.sed("-i", /^.*calendar:.*$/, `\t\tcalendar: "${c}",`, "data/config.js")
     Log.config.system.calendar = c
-    // Log.refresh()
   },
 
   /**
@@ -96,7 +90,6 @@ Log.options = {
   setTimeFormat(f) {
     shell.sed("-i", /^.*timeFormat:.*$/, `\t\ttimeFormat: "${f}",`, "data/config.js")
     Log.config.system.timeFormat = f
-    // Log.refresh()
   },
 
   /**
@@ -107,6 +100,5 @@ Log.options = {
   setWeekStart(d) {
     shell.sed("-i", /^.*weekStart:.*$/, `\t\tweekStart: "${d}",`, "data/config.js")
     Log.config.system.weekStart = d
-    // Log.refresh()
   }
 }
