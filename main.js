8e9c82c95522d4987fb845419a9b5051c6a25d42
Josh Avanier <joshavanier@gmail.com>
2017-12-18 22:47:02 +0700

    Add fadein

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ const url = require('url')
 var win
 
 function createWindow () {
-  win = new BrowserWindow({width: 1000, height: 610, backgroundColor: '#f8f8f8', resizable:true, autoHideMenuBar: true, frame: false})
+  win = new BrowserWindow({width: 1000, height: 610, backgroundColor: '#000', resizable:true, autoHideMenuBar: true, frame: false})
 
   win.loadURL(`file://${__dirname}/index.html`)
 
