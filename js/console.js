fe5128e9262d2e359902f0b521e6bf036958b0bc
Hunor Karamán <hunix94@gmail.com>
2018-01-10 11:22:07 +0200

    quit/exit command

--- js/console.js
+++ js/console.js
@@ -43,6 +43,9 @@ Log.console = {
       case 'invert':
         Log.console.invert();
         break;
+	  case 'quit': case 'exit':
+	  	app.quit();
+	  	break;
       default:
         return
     }
