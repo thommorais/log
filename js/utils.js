43aac7b96f3f0c21f9069da172037695e94df5ac
Josh Avanier <joshavanier@gmail.com>
2018-01-11 08:49:05 +0700

    Switch to const

--- js/utils.js
+++ js/utils.js
@@ -1,23 +1,17 @@
-let isObject = a => typeof(a) === 'object'
-let isNumber = a => typeof(a) === 'number'
-let isString = a => typeof(a) === 'string'
-let isEmpty = a => a.length === 0
-let isUndefined = a => a === undefined
-
-let write = (e, c) => {
-  c = isNumber(c) ? c.toFixed(2) : c
-  // c = c.slice(-2) === '00' ? Number(c).toFixed(0) : c
-  document.getElementById(e).innerHTML = c
-}
-let clear = e => document.getElementById(e).innerHTML = ''
-let create = e => document.createElement(e)
-let isValidArray = a => isObject(a) && !isEmpty(a)
-let isNumArray = a => isObject(a) && a.every(isNumber)
-
-let hasEntries = a => isValidArray(a) && typeof(a[0]) === 'object'
-
-let append = (e, c) => document.getElementById(e).appendChild(c)
-let contains = (s, a) => a.indexOf(s.split(' ')) > -1 ? true : false
-let exists = e => document.getElementById(e) !== null
-let hide = e => document.getElementById(e).style.display = 'none'
-let show = e => document.getElementById(e).style.display = 'block'
+const isObject = a => typeof(a) === 'object'
+const isNumber = a => typeof(a) === 'number'
+const isString = a => typeof(a) === 'string'
+const isEmpty = a => a.length === 0
+const isUndefined = a => a === undefined
+const write = (e, c) => document.getElementById(e).innerHTML = c
+const clear = e => document.getElementById(e).innerHTML = ''
+const create = e => document.createElement(e)
+const isValidArray = a => isObject(a) && !isEmpty(a)
+const isNumArray = a => isObject(a) && a.every(isNumber)
+const hasEntries = a => isValidArray(a) && isObject(a[0])
+const append = (e, c) => document.getElementById(e).appendChild(c)
+const contains = (s, a) => a.indexOf(s.split(' ')) > -1 ? true : false
+const exists = e => document.getElementById(e) !== null
+const hide = e => document.getElementById(e).style.display = 'none'
+const show = e => document.getElementById(e).style.display = 'block'
+const isNull = e => e === null
