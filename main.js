6b637d42e25b95be4119b0bd9b8693f4f8f505bd
Josh Avanier <joshavanier@gmail.com>
2018-01-18 22:21:56 +0700

    Update window size

--- main.js
+++ main.js
@@ -2,12 +2,20 @@ const {app, BrowserWindow, webFrame} = require('electron')
 const path = require('path')
 const url = require('url')
 
-if (process.argv[2] && process.argv[2] == "dev") require('electron-reload')(__dirname)
+if (process.argv[2] && process.argv[2] === 'dev') require('electron-reload')(__dirname)
 
 var win
 
 app.on('ready', function() {
-  win = new BrowserWindow({width: 1000, height: 600, backgroundColor: '#0c0c0c', resizable:true, autoHideMenuBar: true, frame: false, icon: `${__dirname}/icon.ico`})
+  win = new BrowserWindow({
+    width: 960,
+    height: 600,
+    backgroundColor: '#0c0c0c',
+    resizable: false,
+    autoHideMenuBar: true,
+    frame: false,
+    icon: `${__dirname}/icon.ico`
+  })
 
   win.loadURL(`file://${__dirname}/src/index.html`)
 
