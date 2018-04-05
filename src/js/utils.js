5827cf7f30e3738cffd989cda472b8df7b2f28c9
Josh Avanier <joshavanier@gmail.com>
2018-04-05 18:15:36 +0700

    ⨯ Delete

--- src/js/utils.js
+++
@@ -1,4 +0,0 @@
-const isObject = a => typeof (a) === 'object';
-const isNumber = a => typeof (a) === 'number';
-const isEmpty = a => a.length === 0;
-const isValidArray = a => isObject(a) && !isEmpty(a);
